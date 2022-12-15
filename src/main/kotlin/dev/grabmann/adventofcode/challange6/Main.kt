5011b9b4eabd570402ad801c5f3648c8cdb20113
Clemens Grabmann <clemens.grabmann@cloudflight.io>
2022-12-15 14:46:16 +0100

    part 1

--- src/main/kotlin/dev/grabmann/adventofcode/challange6/Main.kt
+++ src/main/kotlin/dev/grabmann/adventofcode/challange6/Main.kt
@@ -1,9 +1,13 @@
 package dev.grabmann.adventofcode.challange6
 
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val file = Path("input.txt")
+    val line = file.readLines().first()
+    val firstMarkerIndex =  line.windowed(4, step = 1, partialWindows = false) {
+        it.toSet().size == 4
+    }.indexOfFirst { it }
+    println(4 + firstMarkerIndex)
 }
